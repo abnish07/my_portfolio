330c8ab92343816411326a63502adfb001c178df
Abnish <akp12556@gmail.com>
2020-08-12 00:38:09 +0530

    final portfolio

--- portfolio/src/components/Profile/Project.jsx
+++ portfolio/src/components/Profile/Project.jsx
@@ -6,11 +6,43 @@ import ProjectCard from './ProjectCard';
 
 function Profile(){
   
-    let obj = {
+    let objOnefinestay = {
         images1: '/images/react.svg',
         images2: '/images/html-5.svg',
         images3: '/images/css-5.svg',
         images4: '/images/bootstrap-4.svg',
+        images5: '/images/javascript.svg',
+        images6: '/images/redux.svg',
+    }
+    let objShoppingCart = {
+        images1: '/images/react.svg',
+        images2: '/images/html-5.svg',
+        images3: '/images/css-5.svg',
+        images4: '/images/bootstrap-4.svg',
+        images5: '/images/redux.svg',
+    }
+    let objTodo = {
+        images1: '/images/react.svg',
+        images2: '/images/html-5.svg',
+        images3: '/images/css-5.svg',
+        images4: '/images/bootstrap-4.svg',
+        images5: '/images/redux.svg',
+    }
+    let objChrome = {
+        images2: '/images/html-5.svg',
+        images3: '/images/css-5.svg',
+        images4: '/images/bootstrap-4.svg',
+        images5: '/images/javascript.svg',
+    }
+    let objTimer = {
+        images2: '/images/html-5.svg',
+        images3: '/images/css-5.svg',
+        images4: '/images/bootstrap-4.svg',
+        images5: '/images/javascript.svg',
+    }
+    let objTypewriter = {
+        images2: '/images/html-5.svg',
+        images3: '/images/css-5.svg',
         images5: '/images/javascript.svg',
     }
     return(
@@ -24,7 +56,7 @@ function Profile(){
              projectTitle = "Onefinestay"
              projectImg = '/images/onefinestay_.png'
              projectUrl = "http://finestay.abhishekray.tech/"
-             projectStackImages = {obj}
+             projectStackImages = {objOnefinestay}
              projectDesc ="Online Hotel Booking website, where user can book a variety of
              hotels and make payment" 
              />
@@ -33,7 +65,7 @@ function Profile(){
              projectTitle = "Shopping Cart"
              projectImg = '/images/shopping_cart.png'
              projectUrl="https://shopping-cart-sigma.vercel.app/"
-             projectStackImages = {obj}
+             projectStackImages = {objShoppingCart}
              projectDesc ="Online shopping cart, select your product, Add add them into the cart, put delivery address and order online." 
              />
 
@@ -41,7 +73,7 @@ function Profile(){
              projectTitle = "Todo"
              projectImg = '/images/todo_1.png'
              projectUrl="https://todo-git-master.abnish07.vercel.app/"
-             projectStackImages = {obj}
+             projectStackImages = {objTodo}
              projectDesc ="Add your items into todo, edit and update your items and remove
              when complete." 
              />
@@ -50,23 +82,23 @@ function Profile(){
              <ProjectCard 
              projectTitle = "Chrome Extension"
              projectImg = '/images/tracker.png'
-             projectStackImages = {obj}
+             projectStackImages = {objChrome}
              projectDesc ="A Tracker to track your waste time on your listing websites. Show the total time at the end of the day." 
              />
           
             
              <ProjectCard 
              projectTitle = "Timer"
-             projectImg = '/images/timer_img.png'
+             projectImg = '/images/timer_.png'
              projectUrl="/timer/timer.html"
-             projectStackImages = {obj}
+             projectStackImages = {objTimer}
              projectDesc =" A java scrit project to show the timer and stopwatch. Timer takes input and run. Perform start, stop, pause and reset." 
              />
              <ProjectCard 
              projectTitle = "Typewriter"
              projectImg = '/images/typewriter.png'
              projectUrl="/Typewriter/keyboard.html"
-             projectStackImages = {obj}
+             projectStackImages = {objTypewriter}
              projectDesc ="A javascript project in which user can write, delete, reverse the string and convert the string into password string." 
              />
              
